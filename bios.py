7630a37cfcf6b5460f8f217dba6bc35aaf41d541
Joyce Fang <jofang@ucsd.edu>
2017-02-13 17:01:39 -0800

    imported stuff so it runs

--- bios.py
+++ bios.py
@@ -4,13 +4,16 @@ import requests  # makes a request to web servers to download HTML contents
 from bs4 import BeautifulSoup
 import pandas as pd  # library that gives you access to data frames
 
-import roster.py
+import roster
 
+from imp import reload
+import sys
+import io
+import json
 reload(sys)  # allows encoding in something not ASCII
 sys.setdefaultencoding('utf-8')
 
-page =
-requests.get("http://www.ucsdtritons.com/ViewArticle.dbml?ATCLID=205824389&DB_OEM_ID=5800")
+page = requests.get("http://www.ucsdtritons.com/ViewArticle.dbml?ATCLID=205824389&DB_OEM_ID=5800")
 soup = BeautifulSoup(page.content, 'html.parser')
 page.status_code
 

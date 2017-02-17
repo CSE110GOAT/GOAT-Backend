b27d9b4733cb5b92969437d6c19f714e54d9c2bb
Joyce Fang <jofang@ucsd.edu>
2017-02-17 15:54:43 -0800

    Grabs urls and headlines for each sport archive. Need to do it for every news archive

--- news.py
+++ news.py
@@ -1,3 +1,7 @@
+# Joyce Fang
+# Description: takes in a sports news archive url and grabs all the article 
+# headlines and urls for each sport 
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -5,7 +9,7 @@ import io
 import json
 from urls import news_urls
 
-page = requests.get(news_urls[0])
+page = requests.get(news_urls[0])  # need to change that loops thru each news_urls
 soup = BeautifulSoup(page.content, 'html.parser')
 
 article_tags = soup.select(".oldheadline a")
@@ -13,6 +17,7 @@ article_tags = soup.select(".oldheadline a")
 article_urls = []
 headlines = []
 
+# stores the headlines and article urls
 for a in article_tags:
     if a.has_attr('href'):
         article_urls.append(a['href'])

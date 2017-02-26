06a17661f918ca32ecf88c3acbe8479ef00581cd
Brian Han <bmhan@ucsd.edu>
2017-02-26 05:18:24 -0800

    Scraped everything from the roster page, which includes the stuff that was previously commented out as extra, but is now being scraped; requires updates to header and general comments

--- roster.py
+++ roster.py
@@ -28,48 +28,105 @@ sports = roster_urls
 rosters = []
 
 for sport in sports:
-    #players_images is a list of player image links; note that there are
-    players_images = []
 
     #players_string is a list of player names
     players_string = []
 
+    #players_hAndw is a list of player height and weight
+    players_hAndw = []
+
+    #players_position is a list of player positions
+    players_position = []
+
+    #players_years is a list of player years
+    players_years = []
+
+    #players_hometowns is a list of player hometowns
+    players_hometowns = []
+
+    #players_images is a list of player image links; note that there are
+    players_images = []
+
     #bio_links is a list of player bio links.
     bio_links = []
+
 
     page = requests.get(sport)
     soup = BeautifulSoup(page.content, 'html.parser')
 
     players_object = soup.select (".player-name a")
 
-    #Holds the strings for this specific sport on this iteration
+    #players_string holds the strings for this specific sport on this iteration
     temp_string = []
     for s in players_object:
         players_string.append (s.get_text())
         temp_string.append(s.get_text())
-#Debug statement
-#print '\n'.join(players_string)
 
+    #hAndw_string is a list of player heights AND weights
+    #NOTE: spacing of height and weight is off due to how it is entered 
+    # in the html file
+    hAndw_object = soup.select (".height")
+    if len (hAndw_object) != 0:
+        for h in hAndw_object:
+            players_hAndw.append(h.get_text())
+    else :
+        for s in temp_string:
+            players_hAndw.append ("")
+        
+    #positions_string is a list of positions
+    positions_object = soup.select (".position .data")
+    if len (positions_object) != 0:
+        for p in positions_object:
+            players_position.append(p.get_text())
+    else :
+        for s in temp_string:
+            players_position.append ("")
+
+    #player_years is a list of years
+    years_object = soup.select (".year .data")
+    if len (years_object) != 0:
+        for y in years_object:
+            players_years.append(y.get_text())
+    else :
+        for s in temp_string:
+            players_years.append ("")
+
+    #players_hometowns is a list of player hometowns
+    hometowns_object = soup.select (".hometown .data")
+    if len (hometowns_object) != 0:
+        for h in hometowns_object:
+            players_hometowns.append(h.get_text())
+    else :
+        for s in temp_string:
+            players_hometowns.append ("")
+
+    #players_images is a list of player images
     for s in temp_string:
         temp = soup.find("img", {"alt":s})
         players_images.append(temp["src"])
-#Debug statement
-#print '\n'.join(players_images)
 
-#Debug statement
-#bio_links is a list of player bio links.
+
+    #bio_links is a list of player bio links.
     prefix = "www.ucsdtritons.com"
     for s in temp_string:
         temp = soup.find("a", {"title":s})
         bio_links.append(prefix + temp["href"])
+
 #Debug statement
 #print '\n'.join(bio_links)
 
     #Creates a temp list of player names, player images, bio links
     # for each sport, and then adds it to the player ID index
     temp = []
-    for index in range (len(players_string)):
-        temp.append ([players_string[index],players_images [index],
+    for index in xrange (len(players_string)):
+        print players_hAndw[index].replace("\n","").replace("\t","").replace("\\", "")
+        temp.append([
+                players_string[index],
+                players_hAndw[index].replace("\n","").replace("\t","").replace("\\", ""),
+                players_position [index],
+                players_years[index],
+                players_hometowns[index].replace("\n","").replace("\t",""),
+                players_images [index],
                 bio_links[index]])
     rosters.append (temp)
 

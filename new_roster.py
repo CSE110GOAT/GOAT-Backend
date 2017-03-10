5a291a714f5e958c9595cbcc6bfb7ab63912fdfa
Brian Han <bmhan@ucsd.edu>
2017-03-09 20:56:45 -0800

    Roster is now separated by gender

--- new_roster.py
+++ new_roster.py
@@ -1,3 +1,26 @@
+# Author: Brian Han
+# Date: 3/9/17
+# My attempt to parse all the rosters by gender
+# The code creates a list, rosters, that at each index has a sublist of 
+# lists, where each list element in the sublist has a:
+# player name, player height and weight, player position, player year, player
+# hometown, player image link, and player bio link
+#
+# Breakdown:
+#      rosters [0] = list of [player name, player height and weight,
+#                             player position, player year, player hometown,
+#                             player image link, player bio link]'s
+#      rosters [0][0] = first [player name, player height and weight,
+#                             player position, player year, player hometown,
+#                             player image link, player bio link] at ID 0
+#      rosters [0][0][0] = first player name at ID 0 
+#      rosters [0][0][1] = first player height and weight at ID 0 
+#      rosters [0][0][2] = first player position at ID 0 
+#      rosters [0][0][3] = first player year at ID 0 
+#      rosters [0][0][4] = first player hometown at ID 0 
+#      rosters [0][0][5] = first image link at ID 0
+#      rosters [0][0][6] = first article link at ID 0
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -14,54 +37,189 @@ sports = roster_urls
 #Index of sport being accessed
 sportIndex = 0
 
+#The index of men, when doing a men/women roster search
+menIndex = 0
+menOffset = 3
 
 #The actual list of rosters
 rosters = []
 
-#for sport in sports:
+#The indexes where there are men/women separation
+MenWomen = [2, 3, 7, 9, 13, 14, 18, 20 ] 
 
-#players_string is a list of player names
-players_strings = []
+for sport in xrange(len(sports)):
 
-#players_hAndw is a list of player height and weight
-players_hAndws = []
+    #players_string is a list of player names
+    players_strings = []
 
-#players_position is a list of player positions
-players_positions = []
+    #players_hAndw is a list of player height and weight
+    players_hAndws = []
 
-#players_years is a list of player years
-players_years = []
+    #players_position is a list of player positions
+    players_positions = []
 
-#players_hometowns is a list of player hometowns
-players_hometowns = []
+    #players_years is a list of player years
+    players_years = []
 
-#players_images is a list of player image links; note that there are
-players_images = []
+    #players_hometowns is a list of player hometowns
+    players_hometowns = []
 
-#bio_links is a list of player bio links.
-bio_links = []
+    #players_images is a list of player image links; note that there are
+    players_images = []
 
+    #bio_links is a list of player bio links.
+    bio_links = []
 
-page = requests.get(sports[2])
-soup = BeautifulSoup(page.content, 'html.parser')
+    #Now gets sports based on index instead of object
+    page = requests.get(sports[sport])
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-players_object = soup.select ("#roster-grid-layout")
-players = players_object[0].contents
-print players[1]
-prefix = "www.ucsdtritons.com"
-#In the case we are scrapping men...
-for i in xrange(2, len(players)):
-    print players[i]
-    print type(players[i])
-    """
-    if players[i].has_attr('class'):
-        if tag['class'] == 'gender':
-            break;
-    temp = players[i].find_children()
-    for thingy in temp:
-        if (thingy.has_attr('href')):
-            bio_links.append(prefix + temp["href"])
-        """
-print bio_links    
+    # Logic to find the men/women index, if required
+    if (sport in MenWomen):
+        players_object = soup.find (id="roster-grid-layout")
+        players = players_object.contents
+
+        #Removes newline characters from the list
+        while True:
+            try:
+                players.remove('\n')
+            except ValueError:
+                break
+
+        for index in xrange(len(players)):
+            if "Women" in players[index].get_text():
+                menIndex = index - menOffset
+                break
+
+    players_object = soup.select (".player-name a")
+
+    #players_string holds the strings for this specific sport on this iteration
+    temp_strings = []
+    for s in xrange(len(players_object)):
+        if (sport in MenWomen):
+            if (sport < 10 and s <= menIndex):
+                players_strings.append (players_object[s].get_text())
+                temp_strings.append(players_object[s].get_text())
+            elif (sport > 10 and s > menIndex): 
+                players_strings.append (players_object[s].get_text())
+                temp_strings.append(players_object[s].get_text())
+        else:
+            players_strings.append (players_object[s].get_text())
+            temp_strings.append(players_object[s].get_text())
+
+    #hAndw_string is a list of player heights AND weights
+    hAndw_object = soup.select (".height")
+    if len (hAndw_object) != 0:
+        for h in xrange(len(hAndw_object)):
+            if (sport in MenWomen):
+                if (sport < 10 and h <= menIndex):
+                    players_hAndws.append(hAndw_object[h].get_text())
+                elif (sport > 10 and h > menIndex): 
+                    players_hAndws.append(hAndw_object[h].get_text())
+            else:
+                players_hAndws.append(hAndw_object[h].get_text())
+    else :
+        for s in temp_strings:
+            players_hAndws.append ("")
         
-    
+    #positions_string is a list of positions
+    positions_object = soup.select (".position .data")
+    if len (positions_object) != 0:
+        for p in xrange(len(positions_object)):
+            if (sport in MenWomen):
+                if (sport < 10 and p <= menIndex):
+                    players_positions.append(positions_object[p].get_text())
+                elif (sport > 10 and p > menIndex): 
+                    players_positions.append(positions_object[p].get_text())
+            else:
+                players_positions.append(positions_object[p].get_text())
+    else :
+        for s in temp_strings:
+            players_positions.append ("")
+
+    #player_years is a list of years
+    years_object = soup.select (".year .data")
+    if len (years_object) != 0:
+        for y in xrange(len(years_object)):
+            if (sport in MenWomen):
+                if (sport < 10 and y <= menIndex):
+                    players_years.append(years_object[y].get_text())
+                elif (sport > 10 and y > menIndex): 
+                    players_years.append(years_object[y].get_text())
+            else:
+                players_years.append(years_object[y].get_text())
+    else :
+        for s in temp_strings:
+            players_years.append ("")
+
+    #players_hometowns is a list of player hometowns
+    hometowns_object = soup.select (".hometown .data")
+    if len (hometowns_object) != 0:
+        for h in xrange(len(hometowns_object)):
+            if (sport in MenWomen):
+                if (sport < 10 and h <= menIndex):
+                    players_hometowns.append(hometowns_object[h].get_text())
+                elif (sport > 10 and h > menIndex): 
+                    players_hometowns.append(hometowns_object[h].get_text())
+            else:
+                players_hometowns.append(hometowns_object[h].get_text())
+    else :
+        for s in temp_strings:
+            players_hometowns.append ("")
+
+    #players_images is a list of player images
+    for s in temp_strings:
+        temp = soup.find("img", {"alt":s})
+        players_images.append(temp["src"])
+
+
+    #bio_links is a list of player bio links.
+    prefix = "www.ucsdtritons.com"
+    for s in temp_strings:
+        temp = soup.find("a", {"title":s})
+        bio_links.append(prefix + temp["href"])
+
+    #Creates a temp list of player names, player images, bio links
+    # for each sport, and then adds it to the player ID index
+
+    temp = []
+    for index in xrange (len(players_strings)):
+        temp.append([
+                players_strings[index],
+                players_hAndws[index].replace("\n","").replace("\t","").replace("\\", ""),
+                players_positions [index],
+                players_years[index],
+                players_hometowns[index].replace("\n","").replace("\t",""),
+                players_images [index],
+                bio_links[index]])
+
+        # puts image into directory and downloads the image
+        # for example, the first baseball player image  would be at
+        # ./rosters/0/0.png , 'wb' means write in binary 
+        with open( "./rosters/"+ str(sportIndex) + "/" + str(index) +".png", 'wb+' ) as out_file:
+            img = urllib2.urlopen(players_images[index])
+            out_file.write( img.read() )
+            out_file.close()
+    rosters.append (temp)
+
+    sportIndex += 1
+
+#Formats the Dataframe in Panda
+players = pd.DataFrame ({
+        "rosters": rosters
+    })
+
+#Debug statements
+
+"""
+print players
+print rosters[0][0]
+print rosters[0][0][0]
+print rosters[0][0][1]
+print rosters[0][0][2]
+"""
+
+#Converting the dataframe to a json file
+players_json = players.to_json()
+with open( "./rosters/roster.json", 'w+') as f:
+        f.write( players_json)
